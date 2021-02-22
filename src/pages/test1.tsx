5ee7739434c71189c603b82e4e320ece6031392e
zhuding.zhang <zhuding.zhang@ly.com>
2021-02-22 09:56:16 +0800

    init

--- src/pages/test1.tsx
+++ src/pages/test1.tsx
@@ -7,16 +7,12 @@ interface Props {
 const Component: React.FC<Props> = props => {
   const {  } = props;
 
-  // 1
-  // 2
-  // 3
+  // init
 
   return (
     <div>
       <input type="text"/>
     </div>
-    // 4
-    // 5
   )
 }
 
